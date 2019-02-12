330d234d9e29b777e15e8ed0a77cd2e9aeb24142
‘RSpidla’ <rspidla@gmail.com>
2019-02-12 21:01:09 +0000

    Added route handler for displaying single url, '/urls/:shortURL'

--- express_server.js
+++ express_server.js
@@ -13,6 +13,17 @@ app.get("/", (req, res) => {
   res.send("Hello and Good Day!");
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  console.log("HERE");
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
+  console.log("Test");
+  // let longURL = urlDatabase[];
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
