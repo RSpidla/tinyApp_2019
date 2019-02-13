f82aa288373dc257481e207ce8f0b06580f526b2
‘RSpidla’ <rspidla@gmail.com>
2019-02-13 02:43:48 +0000

    Final fixes for Added GET and POST route handlers for '/urls/new'

--- express_server.js
+++ express_server.js
@@ -24,7 +24,7 @@ app.get("/urls/:shortURL", (req, res) => {
   // console.log("HERE");
   let templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]
+    longURL: urlDatabase[req.params.shortURL].longURL
   };
   // console.log("Test");
   // let longURL = urlDatabase[];
@@ -45,10 +45,23 @@ app.get("/hello", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  let rng = generateRandomString();
+  urlDatabase[rng] = req.body.longURL;
+  console.log(req.body.longURL);
+  res.status(302).redirect(`/urls/${rng}`);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+// Generate Random Number (rng)
+function generateRandomString() {
+  var result = '';
+  var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (var i = 6; i > 0; --i) {
+      result += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return result;
+};
