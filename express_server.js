3056ec1b5a78d8ec235225ebb96593716d561f4d
‘RSpidla’ <rspidla@gmail.com>
2019-02-13 04:53:32 +0000

    Added in comments for route handlers and functions

--- express_server.js
+++ express_server.js
@@ -7,35 +7,40 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
+// Database ===============================
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// GET Route Handlers ===============================
+
+// Root URL Route ===============================
 app.get("/", (req, res) => {
   res.send("Hello and Good Day!");
 });
 
+// New URL Route ===============================
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 })
 
+// Short URL Route ===============================
 app.get("/urls/:shortURL", (req, res) => {
-  // console.log("HERE");
   let templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL].longURL
   };
-  // console.log("Test");
-  // let longURL = urlDatabase[];
   res.render("urls_show", templateVars);
 });
 
+// URLs Route ===============================
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// URLs Route ===============================
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -44,6 +49,9 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// POST Route Handlers ===============================
+
+// URLs Route ===============================
 app.post("/urls", (req, res) => {
   let rng = generateRandomString();
   urlDatabase[rng] = req.body.longURL;
@@ -51,12 +59,14 @@ app.post("/urls", (req, res) => {
   res.status(302).redirect(`/urls/${rng}`);
 });
 
+// URLs Route ===============================
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+// Helper Functions ===============================
 
-// Generate Random Number (rng)
+// Generate Random Number Function ===============================
 function generateRandomString() {
   var result = '';
   var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
