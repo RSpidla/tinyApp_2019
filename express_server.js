014e7be302067716a79b991b84852c135a3198ae
‘RSpidla’ <rspidla@gmail.com>
2019-02-13 00:53:22 +0000

    Added GET and POST route handlers for '/urls/new', added new view for new route and added generate random number function

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
-var express = require("express");
-var app = express();
-var PORT = 8080;
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -12,6 +15,10 @@ var urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello and Good Day!");
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
 
 app.get("/urls/:shortURL", (req, res) => {
   // console.log("HERE");
@@ -35,6 +42,11 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
 
 app.listen(PORT, () => {
