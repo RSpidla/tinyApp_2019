f2b432113d1e2359c5b87ea6ffabbfbc44e6cc6f
‘RSpidla’ <rspidla@gmail.com>
2019-02-22 05:15:26 +0000

    Deleted commented code and re-formatted code

--- express_server.js
+++ express_server.js
@@ -1,30 +1,22 @@
 const express = require("express");
-// const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
 const app = express();
 const PORT = 8080;
 
 const bodyParser = require("body-parser");
-
 const bcrypt = require('bcrypt');
-
-
 app.use(bodyParser.urlencoded({extended: true}));
-// app.use(cookieParser());
 app.use(cookieSession({
   name: 'session',
   keys: ['qwertyu', 'ertyui'],
-
-  // Cookie Options
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  maxAge: 24 * 60 * 60 * 1000
 }))
-
 app.use(express.static("public"));
 app.use('/images', express.static(__dirname + '/images'));
 
 app.set("view engine", "ejs");
 
-// URLs Database ===============================
+// URLs Database
 let urlDatabase = {
   "b2xVn2": {
     longURL: "http://www.lighthouselabs.ca",
@@ -36,7 +28,7 @@ let urlDatabase = {
   }
 };
 
-// Users Database ===============================
+// Users Database
 const users = { 
   "userRandomID": {
     id: "userRandomID", 
@@ -55,10 +47,7 @@ const users = {
    }
 }
 
-
-// GET Route Handlers ===============================
-
-// Root URL Route ===============================
+// Root URL Route
 app.get("/", (req, res) => {
   const user_id = req.session.user_id;
   if (user_id) {
@@ -69,7 +58,7 @@ app.get("/", (req, res) => {
   }
 });
 
-// New URL Route ===============================
+// New URL Route
 app.get("/urls/new", (req, res) => {
   let user_id = req.session['user_id'];
   let user = users[user_id];
@@ -84,7 +73,7 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 })
 
-// Short URL Requests Route ===============================
+// Short URL Requests Route
 app.get("/u/:shortURL", (req, res) => {
   let longURL = req.params['shortURL'];
 
@@ -104,16 +93,10 @@ console.log(urlDatabase[req.params['shortURL'].longURL]);
   // res.redirect(urlDatabase[req.params['shortURL'].longURL);
 })
 
-// Single and Shortened URL Route ===============================
+// Single and Shortened URL Route
 app.get("/urls/:shortURL", (req, res) => {
   let user_id = req.session['user_id'];
   let user = users[user_id];
-  
-  // console.log(user_id);
-  // // console.log(users);
-  // console.log(users[user_id].user_id);
-  
-  
   let templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL].longURL,
@@ -122,13 +105,11 @@ app.get("/urls/:shortURL", (req, res) => {
   if (user_id === users[user_id]) {
     res.render('urls_show', templateVars);
   } else {
-    // res.status(401).send('Not authorized to edit URLS');
     res.render('urls_show', templateVars);
   }
-  // res.render("urls_show", templateVars);
 });
 
-// URLs Route ===============================
+// URLs Route
 app.get("/urls", (req, res) => {
   let user_id = req.session['user_id'];
   let user = users[user_id];
@@ -145,11 +126,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-// URLs Route ===============================
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// Register Route
 app.get('/register', (req, res) => {
   let user_id = req.session['user_id'];
   let user = users[user_id];
@@ -159,7 +140,7 @@ app.get('/register', (req, res) => {
   res.render("register", templateVars);
 });
 
-// Login ===============================
+// Login
 app.get('/login', (req, res) => {
   let user_id = req.session['user_id'];
   let user = users[user_id];
@@ -170,64 +151,43 @@ app.get('/login', (req, res) => {
   res.render("login", templateVars);
 });
 
-
-// POST Route Handlers ===============================
-
-// Delete URL Route ===============================
+// Delete URL Route
 app.post('/urls/:id/delete', (req, res) => {
-  const currentUser_id = req.session['user_id'];
+  const currentUserID = req.session['user_id'];
   const currentURL = urlDatabase[req.params.id];
-
-  console.log(req.session['user_id']);
-  console.log(urlDatabase[req.params.userID]);
-
-
-  if (!currentUser_id) {
+  if (!currentUserID) {
     res.redirect('/login');
   }
-  if (currentUser_id !== currentURL.user_id) {
+  if (currentUserID !== currentURL.user_id) {
     let templateVars = {
-      user: users[currentUser_id]
+      user: users[currentUserID]
     }
     res.render('urls_denied', templateVars);
   }
   else {
-    delete urlDatabase[req.params.id]
+    delete urlDatabase[req.params.id];
     res.redirect('/urls');
   }
-
-
-
-
-
-
-
-  
 });
 
-// Update URL Route ===============================
+// Update URL Route
 app.post('/urls/:id/edit', (req, res) => {
   const { new_longURL } = req.body;
-  const currentUser_id = req.session['user_id'];
+  const currentUserID = req.session['user_id'];
   const currentURL = urlDatabase[req.params.id];
-
-  console.log(req.session['user_id']);
-  console.log(urlDatabase[req.params.userID]);
-
-
-  if (!currentUser_id) {
+  if (!currentUserID) {
       return res.redirect('/login');
   }
-  if (currentUser_id !== currentURL.userID) {
+  if (currentUserID !== currentURL.userID) {
       let templateVars = {
-          user: users[currentUser_id]
+          user: users[currentUserID]
       }
       return res.render('urls_denied', templateVars);
   } else {
     urlDatabase[req.params.id] = {
-      user_id: currentUser_id, 
+      user_id: currentUserID, 
       longURL: new_longURL, 
-      shortURL: currentUser_id
+      shortURL: currentUserID
     };
     res.redirect('/urls');  
   }
@@ -245,18 +205,17 @@ function checkUser(email, password) {
       }
     }
 }
-// Login Route ===============================
+
+// Login Route
 app.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  // const user = users.user_id;
   let user_id = checkUser(email, password);
-  
   if (user_id) {
     req.session.user_id = user_id;
     res.status(200).redirect('/urls');
   } else {
-      res.status(403)//.send('Status : 403 : Invalid username or password');
+      res.status(403);
       let user = users[user_id];
       let templateVars = {
         user,
@@ -266,31 +225,26 @@ app.post('/login', (req, res) => {
   }
 });
 
-// Logout Route ===============================
+// Logout Route
 app.post('/logout', (req, res) => {
-  // res.clearCookie('user_id', { path: '/' });
   req.session = null;
   res.status(200).redirect('/urls');
 });
 
-// Register Route ===============================
+// Register Route
 app.post('/register', (req, res) => {
   const { email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
   const rng = generateRandomString();
   const user_id = rng;  
-  
   let emailNotRegistered;
-
   for(let key in users) {
-    
     if (users[key].email === email) {
       emailNotRegistered = false;
     } else {
       emailNotRegistered = true;
     }
   }
-
   if (!emailNotRegistered) {
     res.status(400).send("<html><body><strong>Status: 400 Bad Request</strong> - Email Already Registered</body></html>");
   } else if (!email || !password) {
@@ -302,10 +256,9 @@ app.post('/register', (req, res) => {
     req.session.user_id = user_id;
     res.status(200).redirect('urls');
   }
-
 });
 
-// URLs Route ===============================
+// URLs Route
 app.post("/urls", (req, res) => {
   let rng = generateRandomString();
   urlDatabase[rng] = {
@@ -315,25 +268,7 @@ app.post("/urls", (req, res) => {
   res.status(302).redirect(`/urls/${rng}`);
 });
 
-// router.post('/', (req, res) => {
-//   const currentUser = req.session.user_id;
-//   const { longURL } = req.body;
-//   const random = helperFunctions.randomUrl();
-//   const newURL = {
-//     userID: currentUser,
-//     [random]: longURL,
-//     views: 0,
-//     visiterLog: [],
-//     uniqueViews: []
-//   };
-//   urlDatabase[random] = newURL;
-//   res.status(201).redirect(`/urls`);
-// });
-
-
-// Helper Functions ===============================
-
-// Generate Random Number Function ===============================
+// Generate Random Number Function
 function generateRandomString() {
   var result = '';
   var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -343,7 +278,7 @@ function generateRandomString() {
   return result;
 };
 
-// Return URLs of User Function ===============================
+// Return URLs of User Function
 const urlsForUser = (id, data) => {
   let usersURLS = {};
   for (let key in data) {
@@ -354,8 +289,7 @@ const urlsForUser = (id, data) => {
   return usersURLS;  
 };
 
-
-// App Starting Message Route ===============================
+// App Starting Message Route
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
